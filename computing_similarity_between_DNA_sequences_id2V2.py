c1f994061eadaf5902360e5aa7e9fcecb16b4a06
SanneJolijn <s.j.v.d.vorst@student.tue.nl>
2023-05-04 21:39:58 +0200

    Update 4.5 21.40

--- computing_similarity_between_DNA_sequences_id2V2.py
+++ computing_similarity_between_DNA_sequences_id2V2.py
@@ -1,8 +1,8 @@
 import ast
 
 #opening files with the interval lists
-set1 = open('sample_set1.txt')
-set2 = open('sample_set2.txt')
+set1 = open('sample_set3.txt')
+set2 = open('sample_set4.txt')
 
 #reading in text files and putting them into lists
 L1 = set1.readlines() #dit is een lijst van strings
@@ -14,7 +14,7 @@ for i in L1:
     new = ast.literal_eval(i)
     LS1.append(new)
 
-print(LS1)
+#print(LS1)
 
 #make usable lists of the lists of intervals of set 2
 LS2 = []
@@ -22,13 +22,9 @@ for a in L2:
     new2 = ast.literal_eval(a)
     LS2.append(new2)
 
-print(LS2)
+#print(LS2)
 
-#trying the overlap code without using previous code
-LS1D = [([2, 5], [11, 17], [22, 37]), ([110, 117], [255, 263]), ([44, 66], [87, 104], [188, 204]), ([2, 33], [47, 56], [90, 99], [301, 312], [554, 707])]
-LS2D = [([3, 8], [18, 20], [24, 26], [29, 33]), ([117, 120], [240, 256], [259, 307]), ([20, 44], [71, 75], [180, 192], [303, 315]), ([7, 35], [45, 58], [101, 119], [1043, 1352])]
-
-#for ls12
+#for computing similarity
 def has_overlap(l1, l2):
     overlap = 0
     for start1, end1 in l1:
@@ -38,13 +34,19 @@ def has_overlap(l1, l2):
                  break  # Exit the inner loop if overlap is found
     return overlap
 
-results = []
-for s in range(len(LS1D)):
-    result = has_overlap(LS1D[s], LS2D[s])
-    results.append(result)
+#for ls12
+results_12 = []
+for s in range(len(LS1)):
+    result = has_overlap(LS1[s], LS2[s])
+    results_12.append(result)
+print('overlap from LS1 view', results_12)
 
-print(results)
-
+#for ls21
+results_21 = []
+for s in range(len(LS2)):
+    result = has_overlap(LS2[s], LS1[s])
+    results_21.append(result)
+print('overlap from LS2 view', results_21)
 
 #for n1, n2 GAAT GOED!!
 n1 = []
@@ -55,6 +57,26 @@ for tuple in LS1:
 for tuple2 in LS2:
     n2.append(len(tuple2))
 
-#print('nr of intervals LS1', n1)
-#print('nr of intervals LS2', n2)
+print('nr of intervals LS1', n1)
+print('nr of intervals LS2', n2)
 
+#eventual formulas
+LStot = 0
+for s in range(len(results_12)):
+    ls12 = (results_12[s]/max(n1[s],n2[s]))
+    ls21 = (results_21[s]/max(n1[s],n2[s]))
+    #print(ls12)
+    #print(ls21)
+    LS = ((ls12 + ls21)/2)
+    #print(LS)
+    LStot = LStot + LS
+
+#since len(newL1) == len(newL2) always
+finalMetric = LStot / len(LS1)
+print(finalMetric)
+
+#write result to file
+#f = open("finalMetric.txt", "x")
+finalMetric_rounded = ('%.2f' %finalMetric)
+print(finalMetric_rounded)
+#f.write(finalMetric_rounded)
