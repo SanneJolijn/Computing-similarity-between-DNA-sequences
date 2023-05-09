16fdc63375df8b64e4c926b68886a05d71c1e922
SanneJolijn <s.j.v.d.vorst@student.tue.nl>
2023-05-09 13:54:51 +0200

    Update 9.5 13.50

---
+++ computing_similarity_between_DNA_sequences_efficientV2.py
@@ -0,0 +1,19 @@
+#def file_to_tuples(file_name):
+    #with open(file_name) as f:
+        #return [eval(line.strip()) for line in f]
+
+#def has_overlap(interval, comparison_list):
+    #return any(interval[0] <= comp[1] and interval[1] >= comp[0] for comp in comparison_list)
+
+#def similarity(set_1='sample_set1.txt', set_2='sample_set2.txt', outfile='similarity.txt'):
+    #LS1 = file_to_tuples(set_1)
+    #LS2 = file_to_tuples(set_2)
+
+    ##hier nog de ls12 en ls21 bij elkaar pakken en evt nog met inline for loop
+
+    #S_fin = sum(LS12_list) / len(LS1)
+
+    #with open(outfile, 'w') as f:
+        #f.write('{:.2f}'.format(S_fin))
+
+    #return S_fin
